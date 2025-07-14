b048ce214a05aaa5a7ef30d2d9abd1474923d9dd
AjayAchugatla <ajayachugatla17@gmail.com>
2025-07-14 15:20:59 +0530

    refactor: Remove AIRoadmapAgent from route.js and update features with new icons and descriptions

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,10 @@
 import { serve } from "inngest/next";
 
 import { inngest } from "@/inngest/client";
-import { AIRoadmapAgent, generateIndustryInsights } from "@/inngest/function";
+import { generateIndustryInsights } from "@/inngest/function";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [generateIndustryInsights,
-    AIRoadmapAgent
-
   ],
 });
--- data/features.js
+++ data/features.js
@@ -1,11 +1,11 @@
-import { BrainCircuit, Briefcase, LineChart, ScrollText } from "lucide-react";
+import { BarChart, BrainCircuit, Briefcase, LineChart, MicroscopeIcon, Route, ScrollText } from "lucide-react";
 
 export const features = [
   {
     icon: <BrainCircuit className="w-10 h-10 mb-4 text-primary" />,
     title: "AI-Powered Career Guidance",
     description:
-      "Get personalized career advice and insights powered by advanced AI technology.",
+      "Get personalized career insights powered by  AI technology.",
   },
   {
     icon: <LineChart className="w-10 h-10 mb-4 text-primary" />,
@@ -17,5 +17,20 @@ export const features = [
     icon: <ScrollText className="w-10 h-10 mb-4 text-primary" />,
     title: "Smart Resume Creation",
     description: "Generate ATS-optimized resumes with AI assistance.",
+  },
+  {
+    icon: <Route className="w-10 h-10 mb-4 text-primary" />,
+    title: "Roadmap Generator",
+    description: "Create personalized learning and career roadmaps tailored to your goals.",
+  },
+  {
+    icon: <MicroscopeIcon className="w-10 h-10 mb-4 text-primary" />,
+    title: "Resume Analyser",
+    description: "Get instant feedback and suggestions to improve your resume.",
+  },
+  {
+    icon: <BarChart className="w-10 h-10 mb-4 text-primary" />,
+    title: "Mock Quiz",
+    description: "Practice with mock quizzes to prepare for interviews and assessments.",
   },
 ];
